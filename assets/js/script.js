de26ccf847b3307ea6760e56cf68eda92bb7b5ef
TimMcMullan <mcmullant@gmail.com>
2022-04-11 22:45:40 -0400

    fixed jquery error

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,54 +10,54 @@ function placeDataOnPage(url) {
 
         console.log(globalApiData);
 
-        $('day1 .city span').text(data.city.name);
-        $('day1 .temp span').text(data.list[0].main.temp);
-        $('day1 .humidity span').text(data.list[0].main.humidity);
-        $('day1 .windspeed span').text(data.list[0].main.windspeed);
-        // $('day1 .uv_index span').text(data.list[0].main.uv_index);
-        $('day1 .feelslike span').text(data.list[0].main.feels_like);
+        $('#day1 .city span').text(data.city.name);
+        $('#day1 .temp span').text(data.list[0].main.temp);
+        $('#day1 .humidity span').text(data.list[0].main.humidity);
+        $('#day1 .windspeed span').text(data.list[0].main.windspeed);
+        // $('#day1 .uv_index span').text(data.list[0].main.uv_index);
+        $('#day1 .feelslike span').text(data.list[0].main.feels_like);
 
-        $('day2 .city span').text(data.city.name);
-        $('day2 .temp span').text(data.list[0].main.temp);
-        $('day2 .humidity span').text(data.list[0].main.humidity);
-        $('day2 .windspeed span').text(data.list[0].main.windspeed);
-        // $('day2 .uv_index span').text(data.list[0].main.uv_index);
-        $('day2 .feelslike span').text(data.list[0].main.feels_like);
+        $('#day2 .city span').text(data.city.name);
+        $('#day2 .temp span').text(data.list[1].main.temp);
+        $('#day2 .humidity span').text(data.list[1].main.humidity);
+        $('#day2 .windspeed span').text(data.list[1].main.windspeed);
+        // $('#day2 .uv_index span').text(data.list[1].main.uv_index);
+        $('#day2 .feelslike span').text(data.list[1].main.feels_like);
 
-        $('day3 .city span').text(data.city.name);
-        $('day3 .temp span').text(data.list[0].main.temp);
-        $('day3 .humidity span').text(data.list[0].main.humidity);
-        $('day3 .windspeed span').text(data.list[0].main.windspeed);
-        // $('day3 .uv_index span').text(data.list[0].main.uv_index);
-        $('day3 .feelslike span').text(data.list[0].main.feels_like);
+        $('#day3 .city span').text(data.city.name);
+        $('#day3 .temp span').text(data.list[2].main.temp);
+        $('#day3 .humidity span').text(data.list[2].main.humidity);
+        $('#day3 .windspeed span').text(data.list[2].main.windspeed);
+        // $('#day3 .uv_index span').text(data.list[2].main.uv_index);
+        $('#day3 .feelslike span').text(data.list[2].main.feels_like);
 
-        $('day4 .city span').text(data.city.name);
-        $('day4 .temp span').text(data.list[0].main.temp);
-        $('day4 .humidity span').text(data.list[0].main.humidity);
-        $('day4 .windspeed span').text(data.list[0].main.windspeed);
-        // $('day4 .uv_index span').text(data.list[0].main.uv_index);
-        $('day4 .feelslike span').text(data.list[0].main.feels_like);
+        $('#day4 .city span').text(data.city.name);
+        $('#day4 .temp span').text(data.list[3].main.temp);
+        $('#day4 .humidity span').text(data.list[3].main.humidity);
+        $('#day4 .windspeed span').text(data.list[3].main.windspeed);
+        // $('#day4 .uv_index span').text(data.list[3].main.uv_index);
+        $('#day4 .feelslike span').text(data.list[3].main.feels_like);
 
-        $('day5 .city span').text(data.city.name);
-        $('day5 .temp span').text(data.list[0].main.temp);
-        $('day5 .humidity span').text(data.list[0].main.humidity);
-        $('day5 .windspeed span').text(data.list[0].main.windspeed);
-        // $('day5 .uv_index span').text(data.list[0].main.uv_index);
-        $('day5 .feelslike span').text(data.list[0].main.feels_like);
+        $('#day5 .city span').text(data.city.name);
+        $('#day5 .temp span').text(data.list[4].main.temp);
+        $('#day5 .humidity span').text(data.list[4].main.humidity);
+        $('#day5 .windspeed span').text(data.list[4].main.windspeed);
+        // $('#day5 .uv_index span').text(data.list[4].main.uv_index);
+        $('#day5 .feelslike span').text(data.list[4].main.feels_like);
 
         return;
     });
 }
 
-// calls placeDataOnPage() when summoned 
+// calls placeDataOnPage() when summoned
 $(document).ready(function(){
-    $('#user-form').on('submit', function(evt) {
+        $('#user-form').on('submit', function(evt) {
         // prevent page reload 
         evt.preventDefault();
         // city name for function 
         var city = $('#cityname').val();
         // store city searched into memory 
-        $('#searchHistory').append('<li><a class="historyListLink">'+city+'</li>');
+        $('#searchHistory').append('<li><a class="historyListLink"> '+city+'</li>');
         // url build for fetch 
         var url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid="+appid;
 
